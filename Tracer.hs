27dd4b11c32148984df75f58cbca296b83b3fd54
Daniel Cousens <daniel210x@gmail.com>
2013-10-07 17:48:29 +1100

    Refactors tracef and sampler
    Extracts the reflection code from sampler and the masking code from
    tracef, simplifying the overall complexity of these functions.

--- Tracer.hs
+++ Tracer.hs
@@ -4,85 +4,76 @@ import Data.Ord
 import Vec
 
 -- constants
-myBits = [38801, 38033, 38033, 42129, 58513, 38033, 38037, 38043, 59281]
+--bitVector = [38801, 38033, 38033, 42129, 58513, 38033, 38037, 38043, 59281] :: [Int]
+bitVector = [247570, 280596, 280600, 249748, 18578, 18577, 231184, 16, 16] :: [Int]
 
--- uh, TODO
-randR :: Fractional a => a
-randR = 0.5
+spheres :: [Vector3 Double]
+spheres = map fst $ filter snd $ zip sphereGrid sphereMask
+          where fPos j k = Vector3 (fromIntegral $ -k) 0 ((fromIntegral $ -j) - 4)
+                sphereGrid = [fPos j k | j <- [0 .. length bitVector], k <- [0 .. 32]]
+                sphereMask = map (\x -> x > 0) [((.&.) b $ shiftL 1 k) | b <- bitVector, k <- [0 .. 32]]
 
 -- ray trace
 data TraceResult = HIT | UPMISS | DOWNMISS deriving Eq
 
-tracef :: Vector3 Double -> Vector3 Double -> Int -> Int -> Int -> (TraceResult, Vector3 Double, Double)
-tracef o d mask j k = if (masked && (q > 0) && (s > 0.01) && (s < pt))
-        then (HIT, vunit (vp + (vscale d s)), s)
-        else (pm, Vector3 0 0 1, pt)
+tracef :: Vector3 Double -> Vector3 Double -> Vector3 Double -> (TraceResult, Vector3 Double, Double)
+tracef o d so =
+        if (s > 0)
+           then (HIT, vunit (oso + (vscale d s)), s)
+           else (pm, Vector3 0 0 1, 1e9)
         where
-          vp = o + (Vector3 (fromIntegral $ -k) 0 ((fromIntegral $ -j) - 4))
-          b = vdot vp d
-          c = (vdot vp vp) - 1
+          p = ((v3z o) / (v3z d))
+          pm = if p > 0 then UPMISS else DOWNMISS
+          pt = if p > 0 then p else 1e9
+
+          (oso, r) = (o + so, 0.5)
+
+          b = vdot oso d
+          c = (vdot oso oso) - r
           q = b * b - c
           s = (-b) - sqrt q
-          masked = ((.&.) mask $ shiftL 1 k) > 0
-          p = -((v3z o) / (v3z d))
-          pm = if 0.01 < p then DOWNMISS else UPMISS
-          pt = if 0.01 < p then p else 1e9
 
 trace :: Vector3 Double -> Vector3 Double -> (TraceResult, Vector3 Double, Double)
-trace o d = minimumBy (comparing (\(m, n, t) -> t)) [tracef o d mask j k | (mask, j) <- zip myBits [0 .. 8], k <- [0 .. 18]]
+trace o d = minimumBy (comparing (\(m, n, t) -> t)) [tracef o d sphere | sphere <- spheres]
 
 -- world sampler
-sampler :: Vector3 Double -> Vector3 Double -> Vector3 Double
+reflect :: Vector3 Double -> Vector3 Double -> Vector3 Double
+reflect v n = vscale (n + v) ((-2) * (vdot v n))
+
+--sampler :: Vector3 Double -> Vector3 Double -> Double -> Vector3 Double
 sampler o d = if (m == UPMISS)
-                then vscale (Vector3 0.7 0.6 1) ((1 - (v3z d)) ** 4)
-                else
-                  if (m == DOWNMISS)
-                     then
-                       if (even (toInteger ((ceiling $ v3x h2) + (ceiling $ v3y h2)))) -- FIXME: Something is going wrong here, or around here
-                          then Vector3 3 1 1
-                          else vscale (Vector3 3 3 3) (b2 * 0.2 + 0.1)
-                     else (Vector3 p p p) + (sampler h r) * 0.5
+              then vscale (Vector3 44.8 38.4 64) ((1 - (v3z d)) ** 4)
+              else if (m == DOWNMISS)
+                   then Vector3 128 128 128
+                   else vscale ((Vector3 32 32 32) + (sampler h r)) 0.5
         where
           (m, n, t) = trace o d
           h = vscale (o + d) t
-          l = vunit $ (Vector3 (9 + randR) (9 + randR) 16) + (-h)
-          nd = vdot n d
-          r = vscale (d + n) (nd * (-2))
-          b = vdot l n
-          (m2, _, _) = trace h l
-          b2 = if (b < 0 || m2 /= UPMISS) then 0 else b
-          h2 = h * 0.2
-          p = if b > 0 then (vdot l r) ** 99 else 0
+          r = reflect d n
 
 -- multi-sampling and view transform
+base = Vector3 16 18 8
 cameraForward = vunit (Vector3 (-6) (-16) 0)
 cameraUp = (vunit ((Vector3 0 0 1) `vcross` cameraForward)) * 0.002
 cameraRight = (vunit (cameraForward `vcross` cameraUp)) * 0.002
 eyeOffset = (cameraUp + cameraRight) * (-256) + cameraForward
 
-sample :: Int -> Int -> Int -> Vector3 Double
-sample x y r = p * 3.5 where
-        base = Vector3 16 18 8
-        rr = (fromIntegral r) / 64 -- FIXME: Use Monte Carlo sampling instead of non-random uniform sampling
-        t = (cameraUp * (rr - 0.5)) * 99 + (cameraRight * (rr - 0.5)) * 99
-        rx = (rr + fromIntegral x)
-        ry = (rr + fromIntegral y)
-        dir = vunit (-t + ((cameraUp * rx) + (cameraRight * ry) + eyeOffset) * 16)
-        p = sampler (base + t) dir
-
-sample64 :: Int -> Int -> Vector3 Double
-sample64 x y = foldl (+) (Vector3 13 13 13) [sample x y r | r <- [0 .. 63]]
+sample :: Int -> Int -> Vector3 Double
+sample x y = p where
+        (fx, fy) = (fromIntegral x, fromIntegral y)
+        dir = vunit ((cameraUp * fx) + (cameraRight * fy) + eyeOffset)
+        p = sampler base dir
 
 -- program
 main :: IO ()
 main = do
-        let pixels = [sample64 (511 - x) (511 - y) | y <- [0 .. 511], x <- [0 .. 511]]
-        toPPM pixels
+        let pixels = [sample x y | y <- [0 .. 511], x <- [0 .. 511]]
+        toPPM 512 512 pixels
 
 -- utils
-toPPM :: [Vector3 Double] -> IO ()
-toPPM pixels = do
-        putStrLn "P3 512 512 255 " -- FIXME: Infer dimensions from parameters... or pass in parameters
-        mapM_ fmtPixel pixels
+toPPM :: Int -> Int -> [Vector3 Double] -> IO ()
+toPPM w h pixels = do
+        putStrLn $ "P3 " ++ (show w) ++ " " ++ (show h) ++ " 255 "
+        mapM_ fmtPixel $ reverse pixels
         where fmtPixel :: Vector3 Double -> IO ()
-              fmtPixel (Vector3 r g b) = putStrLn (show (floor r) ++ " " ++ show (floor g) ++ " " ++ show (floor b))
+              fmtPixel (Vector3 r g b) = putStrLn $ show (floor r) ++ " " ++ show (floor g) ++ " " ++ show (floor b)
